ca8ce66afdb226b54520cb7b36a96cbef7172d6d
Yazeed K. Alasmari <weacgames@gmail.com>
2025-08-02 19:13:45 +0300

    also update publish

--- theTripChalleng/obj/Release/net6.0/theTripChalleng.AssemblyInfo.cs
+++ theTripChalleng/obj/Release/net6.0/theTripChalleng.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("theTripChalleng")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678adb0c543dbe5d58c64cfea00969ef51387c67")]
 [assembly: System.Reflection.AssemblyProductAttribute("theTripChalleng")]
